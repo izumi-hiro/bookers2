a1dec1fdddfe8c72a4a128a13b40943d435bd7d2
izumi-hiro <transformers2728frost@gmail.com>
2022-04-21 14:21:17 +0000

    [Add] user_edit_protect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+  
   def index
     @user = current_user
     @users = User.all
@@ -30,4 +32,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction)
   end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user.id) unless @user == current_user
+  end
+  
 end
