d7c95252026d2759df471fbaea784a20646f6874
izumi-hiro <transformers2728frost@gmail.com>
2022-04-21 12:52:08 +0000

    [Add] book_update_error_message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,10 @@ class BooksController < ApplicationController
     if @book.save
       flash[:notice] = 'You have created book successfully.'
       redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
     end
   end
 
@@ -28,10 +32,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = 'You have updated book successfully.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
   end
   
